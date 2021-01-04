d15ba5e96e62a4d79a13fd959d7f12c5abd23565
paul wiseman <p@ulwiseman.com>
2021-01-04 13:47:11 +0100

    Initial Commit

---
+++ tooltipper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  tooltipper
+//
+//  Created by Paul Wiseman on 04/01/2021.
+//  Copyright © 2021 Paul. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ tooltipper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tooltipper
+//
+//  Created by Paul Wiseman on 04/01/2021.
+//  Copyright © 2021 Paul. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
